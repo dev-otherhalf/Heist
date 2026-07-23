40ece5c35bd5a2b48b496180f59465b32d215d1d
dev-otherhalf <dev@otherhalf.studio>
2026-07-23 12:40:54 +0530

    feat(site-lock-page) WIP: site lock page hover and klaviyo form

--- src/entrypoints/theme.js
+++ src/entrypoints/theme.js
@@ -1,11 +1,13 @@
 import { initLenis } from "../scripts/lenis";
 import { initViewportVideoAutoplay } from "../scripts/viewport-video-autoplay";
 import { initCartDrawerScrollLock } from "../scripts/cart-drawer-scroll-lock";
+import { initSiteLock } from "../scripts/site-lock";
 
 document.addEventListener("DOMContentLoaded", () => {
   initLenis();
   initViewportVideoAutoplay();
   initCartDrawerScrollLock();
+  initSiteLock();
 });
 
 import "../scripts/heist-header";
